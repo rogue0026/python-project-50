69149c965c62e01eed10f809f5e61b847ee27ad8
Paul Nozdrachev <p.nozdra4ev@yandex.ru>
2025-07-29 13:57:05 +0300

    chore: initial project setup
    Add entry point to run project. Add a cli package for a future
    logic.

---
+++ Makefile
@@ -0,0 +1,6 @@
+build:
+	uv build
+
+package-install:
+	uv tool install --force dist/python_project_50-0.1.0-py3-none-any.whl
+
---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,9 @@
+from gendiff.cli import cli
+
+
+def main():
+    cli.run_gendiff()
+
+
+if __name__ == "__main__":
+    main()
