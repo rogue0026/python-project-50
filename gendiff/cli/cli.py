1cf2a51abef02e122a02070b55eb44bd032b6d33
Paul Nozdrachev <p.nozdra4ev@yandex.ru>
2025-07-29 14:16:51 +0300

    feat: add optional argument -f (--format) to configure format output

--- gendiff/cli/cli.py
+++ gendiff/cli/cli.py
@@ -5,6 +5,8 @@ def run_gendiff():
     parser = argparse.ArgumentParser(
             description="Compares two configuration files and shows a difference."
     )
+    parser.add_argument("-f", "--format", help="set format of output")
     parser.add_argument("first_file")
     parser.add_argument("second_file")
     parser.parse_args()
+
