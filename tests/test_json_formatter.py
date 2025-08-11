4601d347b2e3fac7568e82a15df25efc3519ab73
Paul Nozdrachev <p.nozdra4ev@yandex.ru>
2025-08-11 13:12:43 +0300

    fix: fix linter errors

--- tests/test_json_formatter.py
+++ tests/test_json_formatter.py
@@ -9,7 +9,9 @@ def test_on_simple_files(file1_json, file2_json):
 
 
 def test_on_complex_files(complex_json_file1, complex_json_file2):
-    actual = generate_diff(complex_json_file1, complex_json_file2, format="json")
+    actual = generate_diff(complex_json_file1,
+                           complex_json_file2,
+                           format="json")
     with open("tests/test_data/complex_file1_file2_json_expected.txt") as file:
         expected = file.read()
         assert actual == expected
